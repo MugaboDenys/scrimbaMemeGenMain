a81acb7513575d46d3ae03a1e983dc0048213d81
MugaboDenys <mugabodan2020@gmail.com>
2023-03-09 13:07:08 +0200

    Done CSS

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -37,7 +37,11 @@ export default function Meme() {
                     Get a new meme image 🖼
                 </button>
             </div>
-                <img className="meme--image" src={meme.randomImg} alt="" />
+                <div className="meme">
+                    <img className="meme--image" src={meme.randomImg} alt="" />
+                    <h2 className="meme--text top">One does not simply</h2>
+                    <h2 className="meme--text bottom">Walk into Mordor</h2>
+                </div>
         </main>
     )
 }
